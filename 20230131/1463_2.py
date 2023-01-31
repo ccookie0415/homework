ca7f6c9f8b1884f851509abcd74129f3d62efaaf
ccookie0415 <114826382+ccookie0415@users.noreply.github.com>
2023-01-31 17:25:17 +0900

    Update 1463_2.py

--- 20230131/1463_2.py
+++ 20230131/1463_2.py
@@ -3,9 +3,11 @@ class PublicTransport:
         self.name = name
         self.fare = fare
         self.passenger_count = 0
+        self.all_passenger = 0
 
     def get_in(self, passenger_count):
         self.passenger_count += passenger_count
+        self.all_passenger += passenger_count
 
     def get_off(self, passenger_count):
         self.passenger_count -= passenger_count
@@ -13,14 +15,19 @@ class PublicTransport:
     def current_passenger_count(self):
         return self.passenger_count
     
+    def profit(self):
+        return self.fare * self.all_passenger
+    
 bus = PublicTransport("Bus", 2.5)
-print("Before getting in: ", bus.current_passenger_count()) # 0
+print(bus.current_passenger_count()) # 0
 
-bus.get_in(10)
-print("After getting in 10 passengers: ", bus.current_passenger_count()) # 10
+bus.get_in(30)
+print(bus.current_passenger_count()) # 10
 
 bus.get_off(5)
-print("After getting off 5 passengers: ", bus.current_passenger_count()) # 5
+print(bus.current_passenger_count()) # 5
 
 bus.get_off(10)
-print("After getting off 10 passengers: ", bus.current_passenger_count()) # 0  
+print(bus.current_passenger_count()) # 0  
+
+print(bus.profit()) # 75 (30 passengers * fare 2.5)
